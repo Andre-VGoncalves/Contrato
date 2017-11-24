f3d2ca59a8f60ff437bc0d96da8f53130481addf
Andre Vieira Goncalves <andrevieira@brq.com>
2017-11-24 12:04:14 -0200

    verificacao

--- contrato.js
+++ contrato.js
@@ -7,7 +7,15 @@ MetaCoin.deployed().the(function(instance){
     meta = instance;
     return meta.sendCoin(account_two, 10,{from: account_one});
 }).the(function(result) {
-    alert("Transaction successful")
-}).cath(function(e){
+
+    for (var i = 0; i < result.logs.length; i++) {
+        var log= result.logs[i];
+
+        if (log.event == "Transfer") {
+            break;
+        }
+    }
+
+}).cath(function(err){
     
 })
